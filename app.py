6d9cd5d92e80f5b1e387d77c0a6b1c95e1aec2d6
owNer0ff <76239156+owNer0ff@users.noreply.github.com>
2021-04-26 18:10:08 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,57 @@
+import werkzeug
+from flask import Flask, render_template, json, request
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+app = Flask(__name__)
+
+app.config['MYSQL_DATABASE_USER'] = 'jay'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'jay'
+app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+
+
+@app.route('/signUp', methods=['POST', 'GET'])
+def signUp():
+    try:
+        _name = request.form['inputName']
+        _email = request.form['inputEmail']
+        _password = request.form['inputPassword']
+
+        if _name and _email and _password:
+
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            _hashed_password = werkzeug.generate_password_hash(_password)
+            cursor.callproc('sp_createUser', (_name, _email, _hashed_password))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'message': 'User created successfully !'})
+            else:
+                return json.dumps({'error': str(data[0])})
+        else:
+            return json.dumps({'html': '<span>Enter the required fields</span>'})
+
+    except Exception as e:
+        return json.dumps({'error': str(e)})
+    finally:
+        cursor.close()
+        conn.close()
+
+
+if __name__ == "__main__":
+    app.run(port=8080, host='127.0.0.1')
